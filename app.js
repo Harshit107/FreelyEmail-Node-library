d6f347c058e459c788cba764f02f9919c9045589
harshit107 <harshit107.in@gmail.com>
2023-02-12 01:08:15 +0530

    Bug solved

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // const publicEmailSender = require('./src/mailer.js')
 const fetch = require('node-fetch');
+const testDomain = "http://localhost:3000/public/email/notification";
+const domain = "https://send.donot-reply.online/";
+let OTP = domain + "public/email/otpverification";
+const NORMAL_EMAIL = domain +"public/email/notification";
 
-
-const sendEmail =  async function (appName,subject,recipientsEmail,senderEmail, message, HTMLfile) {
+const sendNormalEmail =  async function (appName,subject,recipientsEmail,senderEmail, message, HTMLfile) {
 
     const msgBody = {
         "appName" : appName,
@@ -13,17 +16,51 @@ const sendEmail =  async function (appName,subject,recipientsEmail,senderEmail, 
         "HTMLfile" : HTMLfile
     }
 
-    const res = await fetch('http://localhost:3000/public/email/notification', {
+    const res = await fetch(NORMAL_EMAIL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(msgBody)
     });
+
     const fRes = await res.json();
-    console.log(fRes);
+    return fRes;
     
 }
 
-module.exports = {sendEmail}
+const sendOTP =  async function (appName,subject,recipientsEmail,senderEmail, otp, withValidTime) {
+
+    const msgBody = {
+        "appName" : appName,
+        "subject" : subject,
+        "recipientsEmail" : recipientsEmail,
+        "senderEmail" : senderEmail,
+        "otp" : otp,
+        "withValidTime" : withValidTime
+    }
+
+    const res = await fetch(OTP, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(msgBody)
+    });
+
+    const fRes = await res.json();
+    console.log(fRes);
+    return fRes;
+    
+}
+
+const EmailSender = {
+    sendNormalEmail,
+    sendOTP
+}
+
+
+// sendOTP("mmt", "This is just a testing email", "harshituem@gmail.com","deadline","12345","5");
+
+module.exports = EmailSender
 
