c0fe16802e69625b3aee13b3ad17b152f5ff6718
harshit107 <harshit107.in@gmail.com>
2023-10-21 02:11:27 +0530

    removed OTP request

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const testDomain = "http://localhost:3000/";
 const domain = "https://email.api.harshit107.tech/"; // changed
 
 let OTP = domain + "public/email/verification/otp";
-let OTPRequest = domain + "public/email/verification/otp/request";
 const NORMAL_EMAIL = domain + "public/email/notification";
 let LINK = domain + "public/email/verification/link";
 
@@ -22,8 +21,8 @@ const sendEmail = async function (data) {
     body: JSON.stringify(data),
   });
 
-  const fRes = await res.json();
-  return fRes;
+  return await res.json();
+
 };
 
 const sendOTP = async function (data) {
@@ -38,27 +37,9 @@ const sendOTP = async function (data) {
     body: JSON.stringify(data),
   });
 
-  const fRes = await res.json();
-  return fRes;
+  return await res.json();
 };
 
-// ---------------- Request OTP````````````````
-
-const sendOTPRequest = async function (data) {
-
-  if (!data) throw new Error("Enter valid email body");
-
-  const res = await fetch(OTPRequest, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  });
-
-  const fRes = await res.json();
-  return fRes;
-};
 
 
 const sendLink = async function (data) {
@@ -72,10 +53,8 @@ const sendLink = async function (data) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  });
-  
-  const fRes = await res.json();
-  return fRes;
+  });  
+  return await res.json();
 };
 
 const EmailSender = {
